6c00f4ef3beca28a0405b5c21f381c7a38f3f152
Yevgeni Shapiro <23049337+crypterio-yevgeni@users.noreply.github.com>
2025-06-13 00:40:49 +0300

    update

--- main.tf
+++ main.tf
@@ -98,51 +98,15 @@ resource "aws_instance" "instance" {
       "sudo helm version"
     ]
   }
+
+  provisioner "file" {
+   source      = "./local.tf.tf"
+   destination = "/tmp/local.tf.tf"
+}
+
   provisioner "remote-exec" {
   inline = [
-    "cat <<EOF | sudo tee -a /tmp/local-config.tf",
-    "provider "kind" {
-}
-
-provider "kubernetes" {
-  config_path = pathexpand(var.kind_cluster_config_path)
-}
-
-resource "kind_cluster" "default" {
-  name            = var.kind_cluster_name
-  kubeconfig_path = pathexpand(var.kind_cluster_config_path)
-  wait_for_ready  = true
-
-  kind_config {
-    kind        = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-
-    node {
-      role = "control-plane"
-      image = "kindest/node:v1.23.4"
-
-      kubeadm_config_patches = [
-        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
-      ]
-      extra_port_mappings {
-        container_port = 80
-        host_port      = 80
-      }
-      extra_port_mappings {
-        container_port = 443
-        host_port      = 443
-      }
-    }
-
-  }
-}
-",
-    "EOF"
-  ]
-}
-  provisioner "remote-exec" {
-  inline = [
-    "terraform init && terraform validate",
+    "cd /tmp/ && terraform init",
     "terraform plan && terraform apply -auto-approve"
   ]
 }
